6b84ed7b66735866b61e0e7e8b8fff6142f7b2fd
Guigoleto <54961064+Guigoleto@users.noreply.github.com>
2019-12-18 00:24:40 -0800

    Add files via upload

--- Assignments/HW 02 - Fahrenheit-Celsius and Retails/Program.cs
+++ Assignments/HW 02 - Fahrenheit-Celsius and Retails/Program.cs
@@ -4,31 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Taxes_and_Retails
+namespace Fahrenheit___Celsius
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double taxWA, retailPrice, salesTax, finalPrice;
-            taxWA = 6.8;
-            Console.WriteLine("In the State of Washington you can pay up to {0}% of the retail price as taxes", taxWA);
-            //Print on the screen the State of Washington tax
-            Console.WriteLine("Insert the Retail Price of the product that you want to buy");
-            //Ask the user to put the retail price of the item that he wants to buy
-            retailPrice =Math.Round(Convert.ToDouble(Console.ReadLine()),2);
-            //Read the input and convert it from a string to a double literal
-            //Used the Math.Round to make it more realistic
-            salesTax = Math.Round((taxWA * retailPrice) / 100, 2);
-            //Calculates the taxes over the retail price
-            //Use the Math.Round to make it more realistic
-            finalPrice = retailPrice + salesTax;
-            //Calculates the final price of the item, which is the retail price plus the sales taxes
-            Console.WriteLine("The product costs U${0} and the sales taxes over that price is U${1}",retailPrice,salesTax);
-            //Print the Retail Price and the Sales Taxes so the user can see it separately
-            Console.WriteLine("Therefore, the final price for that product is U${0}", finalPrice);
-            //Gives the final price 
-
+            double C, F;
+            Console.WriteLine("Put a temperature here in Fahrenheits Degrees and the program will convert it to Celsius Degrees");
+            F = Convert.ToDouble(Console.ReadLine());
+            //The Console.Readline() makes the program more interactive
+            //The Convert.ToDouble is necessary to convert the input (that comes as a string) to a double
+            
+            C = Math.Round(5 * (F - 32) / 9,1);
+            //The math operation above that is slightly different from the one that was on moodle
+            //The Reason it is different is that original "C=5/9 (F-32)" formula would always result in a 0 on C#
+            //Unless we change the math operation 5/9, because in that format, the program would fix the output problem
+            //The Math.Round allows you to make a round up in a math operation that have decimals
+            //And I chose it to make a better conversion
+             
+            Console.WriteLine("The temperature {0}F is equivalent to {1}C", F, C);
+            //In the statement above I just printed the F variable in the place of the {0} and the C variable output in the place {1}
         }
+        // 5/9 ; 5.0/9.0
     }
 }
