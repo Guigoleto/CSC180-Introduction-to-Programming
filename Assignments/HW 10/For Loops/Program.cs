33f5b2f2a332b230c764a5af0bbb442fc4c348e0
Guigoleto <54961064+Guigoleto@users.noreply.github.com>
2019-12-18 00:53:49 -0800

    Add files via upload

---
+++ Assignments/HW 10/For Loops/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace HW_10___Extra_Points
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WordCount();
+
+            //LookAndSay();
+        }
+
+        // Unfinished
+        static void NthLargest ()
+        {
+            Console.WriteLine("Enter a string of number separated by space");
+
+            string[] array = Console.ReadLine().Split();
+            int[] number_array = new int[array.Length];
+            int[] largest = new int[3];
+            for (int u = 0 ; u < array.Length; u++)
+            {
+                number_array[u]=Convert.ToInt32(array[u]);
+            }
+        }
+
+        // Unfinished
+        static void WordCount()
+        {
+            StreamReader input = new StreamReader("input.txt");
+            string[] lines = File.ReadAllLines("input.txt");
+            int lines_counter = 0;
+            for (int i =0; i < lines.Length; i++)
+            {
+                lines_counter++;
+            }
+
+            Console.WriteLine(lines_counter); 
+        }
+    }
+}
