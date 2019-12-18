a5aec289c7a978105a2c1834bca26eec0b644691
Guigoleto <54961064+Guigoleto@users.noreply.github.com>
2019-12-18 00:21:08 -0800

    Add files via upload

---
+++ Assignments/HW 01 - Hello, World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello__World___Guilherme_Costa_Campos
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
